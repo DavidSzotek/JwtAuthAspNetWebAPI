cd5ec5178bde16828a0b7c0c424b50ae7586342d
David Szotek <david.szotek.vs@gmail.com>
2024-02-20 09:32:46 +0100

    Creating DTOs and Static roles

---
+++ JwtAuthAspNetWebAPI/Dtos/UpdatePermissionDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JwtAuthAspNetWebAPI.Dtos
+{
+    public class UpdatePermissionDto
+    {
+        [Required(ErrorMessage = "User name is required")]
+        public string UserName { get; set; }
+    }
+}
